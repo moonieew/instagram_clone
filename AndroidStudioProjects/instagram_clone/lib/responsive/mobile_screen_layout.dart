1be0578c318ae29b63366f63cbc87811fe839bc6
moonieew <nguyet.le@kyanon.digital>
2023-04-15 10:25:29 +0700

    menu bar

--- AndroidStudioProjects/instagram_clone/lib/responsive/mobile_screen_layout.dart
+++ AndroidStudioProjects/instagram_clone/lib/responsive/mobile_screen_layout.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
 
 class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({Key? key}) : super(key: key);
@@ -11,6 +13,25 @@ class MobileScreenLayout extends StatefulWidget {
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
   String username = "";
+  int _page = 0;
+  late PageController pageController;
+  // PageController? pageController;
+
+  void dispose() {
+    super.dispose();
+    // pageController.dispose();
+  }
+
+  void navigationTapped(int page) {
+    // pageController.jumpToPage(page);
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+ 
 
   @override
   void initState() {
@@ -32,8 +53,63 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('$username'),
+      // body: Center(
+      //   child: Text('$username'),
+      // ),
+      body: PageView(
+        children: [
+          Text('feed'),
+          Text('search'),
+          Text('add post'),
+          Text('notif'),
+          Text('profile'),
+        ],
+        physics: const NeverScrollableScrollPhysics(),
+        // controller: pageController,
+        onPageChanged: onPageChanged,
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        backgroundColor: mobileBackgroundColor,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                color: _page == 0 ? primaryColor : secondaryColor,
+              ),
+              label: '',
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                color: _page == 1 ? primaryColor : secondaryColor,
+              ),
+              label: '',
+              backgroundColor: primaryColor),
+          // BottomNavigationBarItem(
+          //     icon: Icon(Icons.home), label: '', backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.add_circle,
+                color: _page == 2 ? primaryColor : secondaryColor,
+              ),
+              label: '',
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite,
+                color: _page == 3 ? primaryColor : secondaryColor,
+              ),
+              label: '',
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+                color: _page == 4 ? primaryColor : secondaryColor,
+              ),
+              label: '',
+              backgroundColor: primaryColor),
+        ],
+        onTap: navigationTapped,
       ),
     );
   }
